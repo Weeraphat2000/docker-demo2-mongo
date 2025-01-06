dcf1917bf4658ff8b1c5ca080226a1e91c877abb
Weeraphat2000 <weeraphat.main@gmail.com>
2025-01-07 00:18:20 +0700

    update tag

--- src/schemas/cat.schema.ts
+++ src/schemas/cat.schema.ts
@@ -1,4 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNumber, IsString } from 'class-validator';
 import { HydratedDocument } from 'mongoose';
 
@@ -7,14 +8,32 @@ export type CateDocument = HydratedDocument<Cat>;
 @Schema()
 export class Cat {
   @Prop()
+  @ApiProperty({
+    title: 'name',
+    description: 'The name of a cat',
+    example: 'Kitty',
+    type: String,
+  })
   @IsString()
   name: string;
 
   @Prop()
+  @ApiProperty({
+    title: 'age',
+    description: 'The age of a cat',
+    example: 2,
+    type: Number,
+  })
   @IsNumber()
   age: number;
 
   @Prop()
+  @ApiProperty({
+    title: 'breed',
+    description: 'The breed of a cat',
+    example: 'Persian',
+    type: String,
+  })
   @IsString()
   breed: string;
 }
